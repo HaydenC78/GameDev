b6b86ae7c16a0af250efc79e12b65f12ef0e7a27
Hayden <haydenlichang@gmail.com>
2026-03-20 15:29:18 -0700

    playerUpdate

--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,14 @@
+import com.badlogic.gdx.Input;
+
 public class Player extends GameObject {
     private double x;
     private double y;
     private int speed = 200;
 
     public Player(double x, double y, int width, int height,String imagePath){
-        super(x,y, width:50, height:50, imagePath:assets/ships.png);
+        super(x,y, 50, 50, "assets/ships.png");
+    }
+    public void move(double deltaTime) {
+        if (Input.Keys.LEFT  )
     }
 }
